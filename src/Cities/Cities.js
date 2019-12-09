bfd97ef8e911342e090baae90ff6320816267e64
toth1janost718 <38955943+toth1janost718@users.noreply.github.com>
2019-12-09 21:29:36 +0100

    Add files via upload

--- src/Cities/Cities.js
+++ src/Cities/Cities.js
@@ -2,15 +2,53 @@ import React from 'react'
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    ScrollView,TouchableWithoutFeedback
 }from 'react-native'
 
+import {colors} from '../theme'
 export default class Cities extends React.Component{
+static navigationOption={
+    title:'Cities',
+    headerTitleStyle:{
+        color:'white',
+        fontSize:20,
+        fontWeight:'400'
+    }
+}
+    viewCity=(city)=>{this.props.navigation.navigate('City',{city})
+}
     render(){
         return(
-            <View>
-                <Text>Hello from Cities</Text>
-            </View>
+            <ScrollView>
+                
+                <View>
+                    <TouchableWithoutFeedback onPress={()=>this.viewCity(city)}
+                    >
+                      <View style={styles.cityContainer}>
+        <Text style={styles.city}>{city.city}</Text>
+        <Text style={styles.country}>{city.country}</Text>
+                          </View>  
+                    </TouchableWithoutFeedback>
+                </View>
+
+            </ScrollView>
+            
         )
     }
 }
+
+const styles =StyleSheet.create({
+    cityContainer:{
+        padding:10,
+        borderBottomWidth:2,
+        borderBottomColor:colors.primary
+    },
+    city:{
+        fontSize:20
+    },
+    country:{
+        color:'rgba(0,0,0,5)'
+    },
+
+})
--- src/Cities/City.js
+++ src/Cities/City.js
@@ -2,15 +2,134 @@ import React from 'react'
 import {
     View,
     Text,
-    StyleSheet
+    StyleSheet,
+    TextInput,
+    ScrollView,
+    TouchableWithoutFeedback,
+    TouchableOpacity
 }from 'react-native'
 
+
+import {colors} from '../theme'
+import { anyTypeAnnotation } from '@babel/types'
+
 export default class City extends React.Component{
+        static navigationOptions=(props)=>  {
+            return{
+                title:props.navigation.state.params.city.city,
+                headerTitleStyle:{
+                    fontSize:20,
+                    fontWeight:'400'
+                }
+            }
+
+        }
+
+        state={
+            name:'',
+            info:''
+        }
+        onChangeText=(key,value)=>
+        {
+            this.setState({
+                [key]:value
+            })
+            
+        }   
+        addLocation=()=>{
+            if(this.state.name===''|| this.state.info ==='') return
+            const {city}=this.props.navigation.state.params
+            const location ={
+                name:this.state.name,
+                info:this.state.info
+            }
+            this.props.screenProps.addLocation(location,city)
+            this.setState({
+                name:'',
+                info:''
+            })
+        }
+
     render(){
         return(
-            <View>
-                <Text>Hello from City</Text>
+            <View style={{flex :1}}>
+                {
+                    city.location.map((location,index)=>(
+                        <View style={styles.locationContainer}>
+                            <Text style ={styles.name}>{location.name}</Text>
+                            <Text style ={styles.info}>{location.info}</Text>
+
+                        </View>
+                    
+                    ))
+                }
+               <TextInput
+                   value={this.state.name}
+                   placeholder='Location name'
+                   onChangeText={val=>this.onChangeText('name',val)}
+                   style={styles.input}
+                   placeholderTextColor='white'
+
+               />
+               <TextInput
+                   value={this.state.info}
+                   placeholder='Location info'
+                   onChangeText={val=>this.onChangeText('info',val)}
+                   style={[styles.input,styles.input2]}
+                   placeholderTextColor='white'
+
+               />
+               <View style={styles.buttonContainer}>
+                   <TouchableOpacity onPress={this.addLocation}>
+                       <View styles={styles.button}>
+                           <Text style={styles.buttonText}>Add Location</Text>
+
+                       </View>
+
+                   </TouchableOpacity>
+               </View>
             </View>
         )
     }
 }
+
+const styles=StyleSheet.create({
+    locationContainer:{
+        padding:10,
+        borderBottomColor:colors.primary,
+        borderBottomWidth:2
+    },
+    nem:{
+        fontSize:20
+    },
+    info:{
+        color:'rgba(0,0,0,5)'
+    },
+    input:{
+        position:'absolute',
+        height:50,
+        backgroundColor:colors.primary,
+        bottom:104,
+        width:'100%',
+        left:0
+    },
+    input2:{
+        bottom:52
+    },
+    buttonContainer:{
+        position:'absolute',
+        bottom:0,
+        left:0,
+        width:'100%'
+    },
+    button:{
+        height:50,
+        backgroundColor:colors.primary,
+        justifyContent:'center',
+        alignItems:'center'
+    },
+    buttonText:{
+        fontSize:24,
+        color:'white'
+    }
+})
